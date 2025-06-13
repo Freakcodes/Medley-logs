d3a1b0ea807e1b73b3eac4207f68ac617b995ae6
Freakcodes <gabhay408@gmail.com>
2025-06-13 22:29:38 +0530

    navigate issue

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -93,7 +93,7 @@ const Write = () => {
     reset(); 
     setContent('');
     setCoverImage(null);
-    navigate(`/posts/${slug}`)
+    navigate(`/myposts`);
   };
 
   // const handleImageUpload = (e) => {
